2c0db81744865d54a6337aea6f7a1ac66b2a8fd1
KristiVeliu12 <kristileri@gmail.com>
2026-01-30 16:59:28 +0100

    joke api added

---
+++ src/main/java/org/projekti/babai/model/JokeApiResponse.java
@@ -0,0 +1,42 @@
+package org.projekti.babai.model;
+
+public class JokeApiResponse {
+
+    private String type;
+    private String joke;
+    private String setup;
+    private String delivery;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getJoke() {
+        return joke;
+    }
+
+    public void setJoke(String joke) {
+        this.joke = joke;
+    }
+
+    public String getSetup() {
+        return setup;
+    }
+
+    public void setSetup(String setup) {
+        this.setup = setup;
+    }
+
+    public String getDelivery() {
+        return delivery;
+    }
+
+    public void setDelivery(String delivery) {
+        this.delivery = delivery;
+    }
+}
+
